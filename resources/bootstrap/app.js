b801c2ad3391d80a5846713bb2fc4460e4f71a3a
LukeMcCann <developer-lukemccann@outlook.com>
2021-09-05 21:50:10 +0100

    Add dbconfig to app object

--- resources/bootstrap/app.js
+++ resources/bootstrap/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 require('dotenv').config();
+const { mongo : db } = include('/config/database.js');
 
 const path = require('path');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
 app.config = include('/config/app');
+app.dbconfig = db;
 
 app.use(express.static(path.join('./', 'public')));
 app.set('view engine', 'ejs');
